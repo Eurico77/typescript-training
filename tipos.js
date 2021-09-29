6ba806d857fdcd1672db6c5c2f27429608f7f51f
eurico77 <euricomagalhaes61@gmail.com>
2021-09-29 16:20:27 -0300

    testing types of variables

--- tipos.js
+++ tipos.js
@@ -1,2 +1,29 @@
 var tipo = 1;
 console.log(tipo);
+var idade = 22;
+// idade = "22";
+console.log(idade);
+var minhaIdade;
+minhaIdade = 22;
+console.log(typeof minhaIdade);
+// hobbys
+var array = ["futebol", "basquete", "natação"];
+console.log(array);
+var hobbies = ["futebol", "basquete", "natação"];
+console.log(hobbies);
+var tuple = ["futebol", 22];
+console.log(tuple);
+// enums
+var Tipo;
+(function (Tipo) {
+    Tipo[Tipo["FUTEBOL"] = 0] = "FUTEBOL";
+    Tipo[Tipo["BASQUETE"] = 1] = "BASQUETE";
+    Tipo[Tipo["NATACAO"] = 2] = "NATACAO";
+})(Tipo || (Tipo = {}));
+var tipor = Tipo.FUTEBOL;
+console.log(tipor);
+// anny 
+var carro = {
+    cor: "azul",
+    velocidade: 120
+};
--- tipos.ts
+++ tipos.ts
@@ -1,3 +1,58 @@
-const tipo: number = 1;
+const tipo : number = 1;
 
 console.log(tipo);
+
+let idade = 22;
+
+// idade = "22";
+console.log(idade);
+
+let minhaIdade;
+minhaIdade = 22;
+
+console.log(typeof minhaIdade);
+
+
+// hobbys
+const array = ["futebol", "basquete", "natação"];
+console.log(array);
+
+const hobbies: string[] = ["futebol", "basquete", "natação"];
+console.log(hobbies);
+
+const tuple: [string, number] = ["futebol", 22];
+console.log(tuple);
+
+// enums
+enum Tipo { 
+    FUTEBOL, BASQUETE, NATACAO
+}
+
+let tipor: Tipo = Tipo.FUTEBOL;
+console.log(tipor);
+
+// anny 
+
+const carro: any = {
+    cor: "azul",
+    velocidade: 120
+};
+
+// funcoes
+
+const sayHello = (nome: string) => {
+    console.log(`Olá ${nome}`);
+}
+
+
+sayHello("Fulano");
+
+const multiplicar = (numero1: number, numero2: number): number => {
+  return numero1 * numero2;
+
+}
+
+console.log(multiplicar(2, 3));
+
+
+
