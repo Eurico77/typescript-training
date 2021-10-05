b385036b70dd4b9cec98df03253611cb20c4cc26
eurico77 <euricomagalhaes61@gmail.com>
2021-10-05 16:01:53 -0300

    add namespace aninhado

--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,20 +1,21 @@
 // namespaces
 // namespace é um tipo de bloco de código que pode ser usado para agrupar classes, interfaces, funções, etc.
+namespace Geometria {
+  export namespace PI {
 
-namespace PI {
 
+    const PI = 3.14;
 
-const PI = 3.14;
+    export function areaCirc(raio: number): number {
+      return PI * raio * raio;
+    }
 
-export function areaCirc(raio: number): number {
-    return PI * raio * raio;
+    export function areaRet(base: number, altura: number): number {
+      return base * altura;
+    }
+
+  }
 }
 
-export function areaRet(base: number, altura: number): number {
-    return base * altura;
-}
-
-}
-
-console.log(PI.areaCirc(10));
+console.log(Geometria.PI.areaCirc(10));
 console.log(PI.areaRet(10, 20));
