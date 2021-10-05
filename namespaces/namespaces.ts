21f9317a5a7b3c46f6e67a3b4651b01107b6639e
eurico77 <euricomagalhaes61@gmail.com>
2021-10-05 15:55:01 -0300

    add namespace

---
+++ namespaces/namespaces.ts
@@ -0,0 +1,20 @@
+// namespaces
+// namespace é um tipo de bloco de código que pode ser usado para agrupar classes, interfaces, funções, etc.
+
+namespace PI {
+
+
+const PI = 3.14;
+
+export function areaCirc(raio: number): number {
+    return PI * raio * raio;
+}
+
+export function areaRet(base: number, altura: number): number {
+    return base * altura;
+}
+
+}
+
+console.log(PI.areaCirc(10));
+console.log(PI.areaRet(10, 20));
